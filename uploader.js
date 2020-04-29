630504335161108ccb35b0070a63c6bbca9cd73c
ekapratama93 <ekapratama93@gmail.com>
2020-04-29 11:38:44 +0700

    add BUCKET_NAME check

--- uploader.js
+++ uploader.js
@@ -7,9 +7,15 @@ if(process.argv.length < 3){
     console.log("Need file to upload");
     process.exit(1);
 }
+
 const fileName = process.argv[2];
 const bucketName = process.env.BUCKET_NAME;
 const uploadDir = process.env.UPLOAD_DIR;
+
+if(!bucketName){
+    console.log("Need BUCKET_NAME configuration to upload");
+    process.exit(1);
+}
 
 var destination = fileName;
 if(uploadDir){
