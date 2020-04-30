78be10540f0576cd6769831959b30008aa38f5fa
ekapratama93 <ekapratama93@gmail.com>
2020-04-30 14:43:59 +0700

    chain command

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 
 current_time=$(date "+%Y.%m.%d-%H.%M")
 
-node index.js --html=./cloudsploit_$current_time.html
-node uploader.js cloudsploit_$current_time.html
+node index.js --html=./cloudsploit_$current_time.html && \
+node uploader.js cloudsploit_$current_time.html && \
 node slack.js cloudsploit_$current_time.html
